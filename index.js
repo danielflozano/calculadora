ac5d1bf8c2d1701608711c4833f6a5e72ab25afd
Daniel Lozano <danyfriko@gmail.com>
2024-02-25 19:43:05 -0500

    Implementacion de inputs para solicitar datos al usuario

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 
 
 console.log('Calculadora');
+
+// Función principal del programa
+function calculadora() {
+    let opcion = prompt('Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada'),
+        num1 = parseFloat(prompt('Ingrese el primer número:')),
+        num2,
+        resultado;
+  
+    if (opcion !== '5' && opcion !== '7') {
+      num2 = parseFloat(prompt('Ingrese el segundo número:'));
+    }
+  
+    alert(
+      'la operacion es ' +
+        opcion +
+        ' el primer valor es ' +
+        num1 +
+        ' el segundo valor es ' +
+        num2
+    );
+  }
+  
+  // Llamar a la función principal
+  calculadora();
