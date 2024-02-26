2ea1dc036f99fc7d751f779d09f547bf564d4ac0
Daniel Lozano <danyfriko@gmail.com>
2024-02-26 08:12:00 -0500

    Implementacion de la funcion para operar divisiones

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ function restar(num1, num2) {
 }
 
 function multiplicar(num1, num2) {
-  return num1 - num2;
+  return num1 * num2;
+}
+
+function dividir (num1, num2) {
+  return num1 / num2;
 }
 
 // Función principal del programa
